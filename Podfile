af360e7395dae87c32754336740ee5b8e71b9022
higan96 <norihiko.oba@gmail.com>
2019-01-15 15:41:47 +0900

    add code

--- Podfile
+++ Podfile
@@ -2,6 +2,7 @@ platform :ios, '9.0'
 
 target 'RXSwiftScrollView' do
   use_frameworks!
-  pod 'RxSwift'
-  pod 'RxCocoa'
+  pod 'RxSwift', '~> 4.4.0'
+  pod 'RxCocoa', '~> 4.4.0'
+  pod 'SnapKit', '~> 4.2.0'
 end
--- RXSwiftScrollView/ViewController.swift
+++ RXSwiftScrollView/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak private var pageControl: UIPageControl!
+    @IBOutlet weak private var scrollView: UIScrollView!
+    private let bag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let pageWidth = UIScreen.main.bounds.width
+        let pageHeight = scrollView.bounds.height
+        let colors: [UIColor] = [
+            .red, .green, .blue
+        ]
+        scrollView.contentSize = CGSize(width: pageWidth * CGFloat(colors.count), height: pageHeight)
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+
+        colors.enumerated().forEach { index, color in
+            let page = UIView()
+            let offset = CGFloat(index) * pageWidth
+            scrollView.addSubview(page)
+            page.backgroundColor = color
+            page.snp.makeConstraints { make in
+                make.top.equalTo(scrollView)
+                make.left.equalTo(scrollView).offset(offset)
+                make.height.equalTo(scrollView)
+                make.width.equalTo(pageWidth)
+            }
+        }
+        
+        Observable.just(colors.count)
+            .bind(to: pageControl.rx.numberOfPages)
+            .disposed(by: bag)
+        
+        scrollView.rx.didScroll
+            .withLatestFrom(scrollView.rx.contentOffset)
+            .map { Int(round($0.x / pageWidth)) }
+            .bind(to: pageControl.rx.currentPage)
+            .disposed(by: bag)
     }
-
-
 }
 
