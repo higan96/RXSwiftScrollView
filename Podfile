239a5aeb54a300fa45e97d92ad5559cf7813deae
higan96 <norihiko.oba@gmail.com>
2019-01-15 15:11:46 +0900

    init

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+
+target 'RXSwiftScrollView' do
+  use_frameworks!
+  pod 'RxSwift'
+  pod 'RxCocoa'
+end
---
+++ RXSwiftScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RXSwiftScrollView
+//
+//  Created by Norihiko Oba on 2019/01/15.
+//  Copyright © 2019 Norihiko Oba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
